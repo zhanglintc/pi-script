97bdb22bd28b5f9a96fc0dfe11e92721fc111dd2
zhanglintc <zhanglintc623@gmail.com>
2019-03-21 22:42:36 +0800

    remove ali

--- auto-proxy.pl
+++ auto-proxy.pl
@@ -1,12 +1,11 @@
 use 5.016;
 
 # bind 2222
-`sudo autossh -M 888 -fN -o "PubkeyAuthentication=yes" -o "StrictHostKeyChecking=false" -o "PasswordAuthentication=no" -o "ServerAliveInterval 60" -o "ServerAliveCountMax 3" -R 0.0.0.0:2222:localhost:22 -i /home/lane/.ssh/id_rsa lane\@vultr.zhanglintc.work`;
+`sudo autossh -M 1000 -fN -o "PubkeyAuthentication=yes" -o "StrictHostKeyChecking=false" -o "PasswordAuthentication=no" -o "ServerAliveInterval 60" -o "ServerAliveCountMax 3" -R 0.0.0.0:2222:localhost:22 -i /home/lane/.ssh/id_rsa lane\@vultr.zhanglintc.work`;
 
 # bind 9091
-`sudo autossh -M 999 -fN -o "PubkeyAuthentication=yes" -o "StrictHostKeyChecking=false" -o "PasswordAuthentication=no" -o "ServerAliveInterval 60" -o "ServerAliveCountMax 3" -R 0.0.0.0:9091:localhost:9091 -i /home/lane/.ssh/id_rsa lane\@vultr.zhanglintc.work`;
+`sudo autossh -M 1002 -fN -o "PubkeyAuthentication=yes" -o "StrictHostKeyChecking=false" -o "PasswordAuthentication=no" -o "ServerAliveInterval 60" -o "ServerAliveCountMax 3" -R 0.0.0.0:9091:localhost:9091 -i /home/lane/.ssh/id_rsa lane\@vultr.zhanglintc.work`;
 
 # camera
-`sudo autossh -M 998 -fN -o "PubkeyAuthentication=yes" -o "StrictHostKeyChecking=false" -o "PasswordAuthentication=no" -o "ServerAliveInterval 60" -o "ServerAliveCountMax 3" -R 0.0.0.0:2603:localhost:8081 -i /home/lane/.ssh/id_rsa lane\@vultr.zhanglintc.work`;
+`sudo autossh -M 1004 -fN -o "PubkeyAuthentication=yes" -o "StrictHostKeyChecking=false" -o "PasswordAuthentication=no" -o "ServerAliveInterval 60" -o "ServerAliveCountMax 3" -R 0.0.0.0:2603:localhost:8081 -i /home/lane/.ssh/id_rsa lane\@vultr.zhanglintc.work`;
 
-`sudo autossh -M 995 -fN -o "PubkeyAuthentication=yes" -o "StrictHostKeyChecking=false" -o "PasswordAuthentication=no" -o "ServerAliveInterval 60" -o "ServerAliveCountMax 3" -R 0.0.0.0:2803:localhost:8081 -i /home/lane/.ssh/id_rsa lane\@ali.zhanglintc.work`;
